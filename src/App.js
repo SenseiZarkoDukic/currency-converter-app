5699f2fc1a5a9344426de2f694a981ab9f04c08f
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-09-12 18:20:56 +0200

    Call fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
+import { useEffect, useState } from "react";
+
 export default function App() {
+
+  const [curr2, setCurr1] = useState(0)
+  const [amount, setAmount] = useState(0)
+
+  const curr1 = curr2/amount;
+
+  useEffect(function () {
+
+    async function() {
+      try {
+        const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${curr1}&to=${curr2}`);
+
+        if(!res.ok)
+        throw new Error('Something went wrong with fetching currencies')
+
+        const data = await res.json()
+
+        set
+      } catch (err) {
+        console.log(err.message)
+      }
+    }
+  }, []);
+
   return (
     <div>
       <input type="text" />
